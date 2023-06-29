c724dee0aee86888c5a8d27c18ae52d84bc41b0a
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-06-29 20:42:55 +0000

    completed code

---
+++ module_1/ArthimaticOperations.py
@@ -0,0 +1,21 @@
+#input
+repeatNum = int(input('Please enter a two digit number: '))
+
+#output
+result = repeatNum * (3 * 7 * 13 * 37)
+print('Watch it repeat!', result)
+
+print("Let's have some fun with math!")
+
+#inputs
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+
+#calculations
+print(num1 + num2)
+print(num1 * num2)
+print(num1 - num2)
+print('Division', num1/num2)
+print('Int Division', num1//num2)
+print(num1%num2)
+print('Power of: ' + str(num1**num2))
