ba1a04ff119b42c62bd84241187004d3426ff4ea
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-06-29 19:14:35 +0000

    completed code

---
+++ module_1/BirthdayInvitation.py
@@ -0,0 +1,38 @@
+#Input: name, location, date, time
+
+name = input("What is his/her name?")
+location = input("Where the party is going to be held?")
+date = input("What is date of the party?")
+time = input("What time the part is going to start?")
+
+
+#Output
+
+print('To:', name)
+print('I would love to invite you to my birthday party! That would b wonderful to see you there!')
+print("The party will be at", location, "on", date, "at", time, "! Hope to see you there!")
+print("""X X X X X X X X X X X X X X X X X X X X X X X X X X
+X                                                 X
+X                     0   0                       X
+X                     |   |                       X
+X                 ____|___|____                   X
+X              0  |~ ~ ~ ~ ~ ~|   0               X
+X              |  |           |   |               X
+X           ___|__|___________|___|__             X
+X           |/\/\/\/\/\/\/\/\/\/\/\/|             X
+X       0   |       H a p p y       |   0         X
+X       |   |/\/\/\/\/\/\/\/\/\/\/\/|   |         X
+X     __|___|_______________________|___|__       X
+X     |/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/|       X
+X     |                                   |       X
+X     |         B i r t h d a y           |       X
+X     |                                   |       X
+X     |               T o                 |       X
+X     |                                   |       X
+X     |          E S M A E I L            |       X
+X     |                                   |       X
+X     | ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ |       X
+X     |___________________________________|       X
+X                                                 X
+X X X X X X X X X X X X X X X X X X X X X X X X X X""")
+
