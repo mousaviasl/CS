97165754040b67202cb93ff54859ee416cf52f7b
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-06-29 20:01:24 +0000

    completed code

---
+++ module_1/MadLib.py
@@ -0,0 +1,15 @@
+#Inputs
+name = input("Enter a name: ")
+hobby = input("Enter a hobby: ")
+adjevctive = input("Enter an adjective: ")
+love = input("Enter a love: ")
+hate = input("Enter a hate: ")
+personality_1 = input("Enter a personality trait: ")
+personality_2 = input("Enter another personality trait: ")
+philosophy = input("Enter a philosophy: ")
+feeling = input("Enter a feeling: ")
+
+
+#Output:
+
+print(f'Hello, my name is {name}. One thing I would like\n you to know about me is when I am not programing I love to {hobby}.\n I also want you to know that I am a {adjevctive} student who loves {love} but\n absolutely hates {hate}! The best way to describe my personality is {personality_1} and {personality_2}.\n My personal philosophy or a strong belief that I have that guides me through my life is {philosophy}.\n I am {feeling} to learn about programming!')
