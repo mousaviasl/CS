6fe1b48cb84f8b74534b5a599c2f55ee00591e57
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-08-06 18:49:21 +0000

    cc

--- OpenSapceToCode.py/Free1.py
+++ OpenSapceToCode.py/Free1.py
@@ -1,18 +1,2 @@
-#Keeps track of the amount of prime numbers the user enters
-num = int(input("Enter a positive value: -1 to quit"))
-primeCount = 0
-isPrime = True
-while num > 0:
-  if num > 0:
-    for i in range(1, num):
-      if num % i == 0:
-        isPrime = False
-  if isPrime:
-    print(num, "is prime")
-    primeCount += 1
-  else:
-    print(num, "is not prime")
-  num = int(input("Enter a positive value: -1 to quit"))
 
-print("Prime Count: ", primeCount)
 
