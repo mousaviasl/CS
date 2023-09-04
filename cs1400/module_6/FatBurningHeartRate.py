205c745b09adf726409f6a7d0c0ae072c1a73d0f
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-09-04 20:31:56 +0000

    Cc

--- cs1400/module_6/FatBurningHeartRate.py
+++ cs1400/module_6/FatBurningHeartRate.py
@@ -1,19 +1,38 @@
 print("Welcome to the fat burning heart rate and BMI machine!")
 
-try:
-    age = int(input("Enter the adult's age: "))
-    max = 220 - age
-    fat_burning = max * 70/100
-    weight = int(input("Enter your weight in Kilograms: "))
-    height = int(input("Enter your height in meters: : "))
-    BMI = weight / (height**2)
+while True:
+    exception_occurred = False
     
-    if age < 18:
-        raise ValueError("You are so young and our machine cannot calculate your fat-burning heart rate.")
-except 
-print(BMI)
+    try:
+        age = int(input("Enter the adult's age: "))
+        max = 220 - age
+        fat_burning = max * 70/100
+        weight = int(input("Enter your weight in kilograms: "))
+        height = float(input("Enter your height in meters: "))
+        BMI = round((weight / (height**2)), 3)
+        
+        if age < 0:
+            exception_occurred = True
+            raise ValueError("Invalid age. Enter a positive number for your age.")
+        if max <= 0:
+            raise ValueError("Invalid age. You cannot be that old and use this program.")
+        if weight < 0:
+            exception_occurred = True
+            raise ValueError("Invalid weight! Enter a positive number for your weight.")
+        if height < 0:
+            exception_occurred = True
+            raise ValueError("Invalid height! Enter a positive number for your height.")
+    except Exception as e:
+        print(e)
+        exception_occurred = True
+
+    if not exception_occurred:
+        break 
+
+print("BMI:", BMI)
+
 if BMI < 19:
-    print("You BMI is under the normal standard.")
+    print("Your BMI is under the normal standard.")
 elif BMI <= 25:
     print("Your BMI is in the normal range.")
 else:
