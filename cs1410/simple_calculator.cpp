9f9c744c1536d949a41b74f1ad78869efbf7edfc
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-09-11 03:48:02 +0000

    Cc

---
+++ cs1410/simple_calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int loan_amount;
+    int month;
+    double rate;
+    cout << "Please Enter the amount that you need to take a loan: " << endl;
+    cin >> loan_amount;
+    cout << "Please enter the number of months that you want to pay the loan back in: " << endl;
+    cin >> month;
+    cout << "Please enter the interest rate: (i.e. 6.4 %) " << endl;
+    cin >> rate;
+
+    return 0;
+
+}
