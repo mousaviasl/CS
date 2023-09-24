c1011caeb15188dd2a2a60250cc37b169c1dfb37
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-09-24 06:27:05 +0000

    Cc

---
+++ cs1410/Decision_Loops_Demos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int value = 1;
+
+switch (value)
+{
+    case 0:
+        cout << 'a';
+        break;
+    case 1:
+    case 2:
+        cout << 'b';
+        break;
+    default:
+        cout << 'c';
+
+})
