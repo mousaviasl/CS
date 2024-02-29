d38170d516600510993e0b5030b41c9a80e0a983
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2024-02-29 08:33:00 +0000

    Done

---
+++ cs1410/HelloWorld.cpp
@@ -0,0 +1,12 @@
+/* Date: 01/16/2024
+Program Name: HelloWorld
+Time Invested: 2 minutes
+Author: Esmaeil Mousavi*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!!! My name is Esmaeil!" << endl;
+    return 0;
+}
